ec9a1f8a7b1aa24052814e239b04683c425e06d9
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-05-07 14:03:17 -0400

    Working rust socket server

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,30 @@
 use axum::routing::get;
-use serde_json::Value;
-use socketioxide::SocketIo;
+use socketioxide::{
+    extract::SocketRef,
+    SocketIo,
+};
 use tracing::info;
-use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
-
     let (layer, io) = SocketIo::new_layer();
-    io.ns("/", |socket, auth: Value| async move {
-        info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.sid);
-        socket.emit("auth", auth).ok();
 
-        socket.on("message", |socket, data: Value, bin, _| async move {
-            info!("Received event: {:?} {:?}", data, bin);
-            socket.bin(bin).emit("message-back", data).ok();
+    // Register a handler for the default namespace
+    io.ns("/", |s: SocketRef| {
+        // For each "message" event received, send a "message-back" event with the "Hello World!" event
+        s.on("message", |s: SocketRef| {
+            s.emit("message-back", "Hello World!").ok();
         });
-
-        socket.on("message-with-ack", |_, data: Value, bin, ack| async move {
-            info!("Received event: {:?} {:?}", data, bin);
-            ack.bin(bin).send(data).ok();
-        });
-
-        socket.on_disconnect(|socket, reason| async move {
-            info!("Socket.IO disconnected: {} {}", socket.sid, reason);
-        });
-    });
-
-    io.ns("/custom", |socket, auth: Value| async move {
-        info!("Socket.IO connected on: {:?} {:?}", socket.ns(), socket.sid);
-        socket.emit("auth", auth).ok();
     });
 
     let app = axum::Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .layer(layer);
+    .route("/", get(|| async { "Hello, World!" }))
+    .layer(layer);
 
     info!("Starting server");
 
-    Server::bind(&"127.0.0.1:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await?;
-
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    
     Ok(())
 }
