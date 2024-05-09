b9df98bfa4a4301f3d6fa5f7956e6421d28f6a26
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-05-09 18:24:13 -0400

    Go server works now

--- BuntDB/main.go
+++ BuntDB/main.go
@@ -3,37 +3,46 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"time"
 
-	socketio "github.com/googollee/go-socket.io"
+	"github.com/googollee/go-socket.io"
 	"github.com/tidwall/buntdb"
 )
 
 func main() {
-	// Create a new Socket.IO server
-	server, err := socketio.NewServer(nil)
+	fmt.Println("Start server...")
+
+	// Serve a simple HTML page on root path
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Go server is up and running!")
+	})
+
+	// Listen on port 8000
+	ln, err := net.Listen("tcp", ":3001")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer ln.Close()
 
-	// Handle connection events
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("New connection:", s.ID())
-		return nil
-	})
+	// Initialize a new server
+	server := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Server listening on port 3001")
+	}
 
-	// Handle disconnection events
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("Connection closed:", s.ID(), reason)
-	})
+	// Handle '/socket.io/' endpoint
+	http.Handle("/socket.io/", server)
+	go http.Serve(ln, nil)
 
-	// Handle transactions
+	// Handle events
 	server.OnEvent("/", "update", func(s socketio.Conn, txData string) {
 		fmt.Println("Received update request:", txData)
 
-		// Open the data.db file. It will be created if it doesn't exist.
+		// Open the database (use in-memory for this example)
 		db, err := buntdb.Open(":memory:")
 		if err != nil {
 			log.Fatal(err)
@@ -55,11 +64,6 @@ func main() {
 		s.Emit("updateResult", fmt.Sprintf("Update transaction took: %s", elapsed))
 	})
 
-	// Serve the Socket.IO server at /socket.io endpoint
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./public")))
-
-	// Start the HTTP server
-	fmt.Println("Server started at :3001")
-	log.Fatal(http.ListenAndServe(":3001", nil))
+	// Run forever
+	select {}
 }
