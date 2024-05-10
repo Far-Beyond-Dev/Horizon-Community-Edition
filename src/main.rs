a5f1797448b546f5380ea7fa53f20b25369ce496
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-05-10 16:02:57 -0400

    Swapped clients

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use socketioxide::{
     extract::SocketRef,
     SocketIo,
 };
-use SocketIo as SocketIoClient
+use rust_socketio::{ClientBuilder, Payload, RawClient};
 
 use tracing::info;
 
@@ -36,21 +36,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
     async fn run_client() -> Result<(), Box<dyn std::error::Error>> {
-    // Emit a "update" event with transaction data to the Go server
-    let tx_data = "Transaction data goes here";
-    // socket.emit("update", &tx_data).await.expect("Failed to emit event");
-    
-    let (layer, io) = SocketIoClient::new_layer();
-    // Connect to the Socket.IO server running on Go
-    let socket = SocketIoClient::ClientBuilder::new("http://localhost:3001")
+        let socket = ClientBuilder::new("http://localhost:4200")
+        .namespace("/admin")
+        .on("test", |message, _| eprintln!("Error: {:#?}", message))
+        .on("error", |err, _| eprintln!("Error: {:#?}", err))
         .connect()
-        .expect("Failed to connect to server");
-    
-    // Handle incoming events from the GOLANG server (if any)
-    socket.on("updateResult", |res| {
-        println!("Received update result: {:?}", res);
-    })
-    Ok(())
+        .expect("Connection failed");
+
+        Ok(())
     }
 
 async fn run_server() -> Result<(), Box<dyn std::error::Error>> {
