2ce6110395d7fd2dd99befd4c8bb27b52bcc5a71
a catgirl :3 <nyameowmeow@duck.com>
2024-06-16 12:08:44 -0400

    docs: fix up rustdoc in macros.rs

--- src/macros.rs
+++ src/macros.rs
@@ -1,10 +1,10 @@
 #[macro_use]
 mod export {
-    /// Append .on to a specific **mutable** variable so you don't have to.
+    /// Append .on to a **mutable** SocketRef to keep everything concise.
     ///
     /// The first parameter is the variable itself; a socketioxide::extract::SocketRef type.
     /// The second value is the endpoint, eg "/".
-    /// The third value is what to return back.
+    /// The third value is the function to be called.
     ///
     /// # Example
     ///
@@ -14,8 +14,8 @@ mod export {
     ///               "/function", function());
     /// ```
     ///
-    /// Note: this example does not compile becuase you might not have a function literally named
-    /// function() and no variable with a SocketRef type. This is just an example.
+    /// Note: this example does not compile because you may not have such variables or functions.
+    /// This is just an example of how it's meant to be used.
     #[macro_export]
     macro_rules! define_event {
          ($app:expr, $($path:expr, $handler:expr),* $(,)?) => {
