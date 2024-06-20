382d194f82c4dde5faacf1fad1333e09b1b3459e
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-06-20 18:18:04 -0400

    Updated readme, disabled auth subsystem for now

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -1,6 +1,6 @@
 // Register Custom Events
 
-// pub mod auth;
+// pub mod auth; Auth is not yet being used as it needs to be a more flexible
 pub mod chat;
 pub mod leaderboard;
 pub mod player_data;
