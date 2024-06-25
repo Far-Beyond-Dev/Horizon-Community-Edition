941ade3a9cfc43a8bba3b6990fe7cdc13e068fd3
Thiago Goulart <thiagomacenogoulart@gmail.com>
2024-06-24 22:06:00 -0300

    Possible Impls

--- PebbleVault/go/go.mod
+++ PebbleVault/go/go.mod
@@ -3,12 +3,12 @@ module pebble-vault
 go 1.22.4
 
 require (
-	//github.com/tidwall/btree v1.7.0 // indirect
-	//github.com/tidwall/buntdb v1.3.1 // indirect
-	//github.com/tidwall/gjson v1.17.1 // indirect
-	//github.com/tidwall/grect v0.1.4 // indirect
-	//github.com/tidwall/match v1.1.1 // indirect
-	//github.com/tidwall/pretty v1.2.1 // indirect
-	//github.com/tidwall/rtred v0.1.2 // indirect
-	//github.com/tidwall/tinyqueue v0.1.1 // indirect
+	github.com/tidwall/btree v1.7.0 // indirect
+	github.com/tidwall/buntdb v1.3.1 // indirect
+	github.com/tidwall/gjson v1.17.1 // indirect
+	github.com/tidwall/grect v0.1.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/rtred v0.1.2 // indirect
+	github.com/tidwall/tinyqueue v0.1.1 // indirect
 )
--- PebbleVault/go/main.go
+++ PebbleVault/go/main.go
@@ -7,9 +7,10 @@ import "C"
 
 import (
 	"fmt"
+	"log"
 	"unsafe"
-	//"log"
-	//"github.com/tidwall/buntdb"
+
+	"github.com/tidwall/buntdb"
 )
 
 //export Greet
@@ -22,12 +23,66 @@ func GoFree(ptr *C.char) {
 	C.free(unsafe.Pointer(ptr))
 }
 
-func main() {
+//export CreateDB
+func CreateDB() uintptr {
 	// Open the data.db file. It will be created if it doesn't exist.
-	//db, err := buntdb.Open("data.db")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//defer db.Close()
+	db, err := buntdb.Open("data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return uintptr(unsafe.Pointer(db))
+	//return uintptr(uintptr(unsafe.Pointer(db)))
+}
+
+//export CloseDB
+func CloseDB(db uintptr) {
+	// Close the database when done.
+	(*buntdb.DB)(unsafe.Pointer(db)).Close()
+}
+
+//export CreateSpatialIndex
+func CreateSpatialIndex(db uintptr, indexName *C.char, indexKey *C.char) {
+	// db.CreateSpatialIndex("fleet", "fleet:*:pos", buntdb.IndexRect)
+	(*buntdb.DB)(unsafe.Pointer(db)).CreateSpatialIndex(C.GoString(indexName), C.GoString(indexKey), buntdb.IndexRect)
+}
+
+//db.Update(func(tx *buntdb.Tx) error {
+//	tx.Set("fleet:0:pos", "[-115.567 33.532]", nil)
+//	tx.Set("fleet:1:pos", "[-116.671 35.735]", nil)
+//	tx.Set("fleet:2:pos", "[-113.902 31.234]", nil)
+//	return nil
+//})
+
+//export CreateGalaxy
+func CreateGalaxy(db uintptr, key *C.char, value *C.char) {
+	/*
+		func do add Galaxy Data, made it by:
+		Transform data: {Location: {x: 0, y: 0, z: 0},
+		Rotation: {x: 0, y: 0, z: 0},
+		Scale: {x: 0, y: 0, z: 0},
+		Point Data: Array of relative location vectors paired with a brightness value and a color value(RGB):
+		[{x: 0, y: 0, z: 0, brightness: 0, color: {r: 0, g: 0, b: 0}}]}
+	*/
+	(*buntdb.DB)(unsafe.Pointer(db)).Update(func(tx *buntdb.Tx) error {
+		tx.Set(C.GoString(key), C.GoString(value), nil)
+		return nil
+	})
+}
+
+//export GetKNearestGalaxys
+func GetKNearestGalaxys(db uintptr, key *C.char) *C.char {
+	var result string
+	(*buntdb.DB)(unsafe.Pointer(db)).View(func(tx *buntdb.Tx) error {
+		tx.Nearby("galaxy", C.GoString(key), func(key, val string, dist float64) bool {
+			result += key + ":" + val + ","
+			return true
+		})
+		return nil
+	})
+	return C.CString(result)
+}
+
+func main() {
+	// Prevent main from exiting immediately.
+	select {}
 }
