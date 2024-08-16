9b88c49b96deefeadad6bb65fc8448310e4453d9
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-08-16 18:50:54 +0000

    Updated for custom transform struct

--- src/players.rs
+++ src/players.rs
@@ -92,24 +92,25 @@ pub fn init(socket: SocketRef, players: Arc<Mutex<Vec<Player>>>) {
 /// This function assumes specific data structures for the incoming data and may fail silently
 /// if the expected fields are missing or in an unexpected format.
 pub fn update_player_location(socket: SocketRef, data: Data<Value>, players: Arc<Mutex<Vec<Player>>>) {
-    println!(
-        "Received event: UpdatePlayerLocation with data: {:?}",
-        data.0  // Access the inner Value
-    );
-    // Extract location from data
+    println!("Received event: UpdatePlayerLocation with data: {:?}", data.0);
+
     if let Some(transform) = data.0.get("transform").and_then(|t| t.as_object()) {
         let mut players = players.lock().unwrap();
         if let Some(player) = players.iter_mut().find(|p| p.socket.id == socket.id) {
-            // Do the actual parsing
-            if let (Some(rotation), Some(translation), Some(scale3d)) = (
-                transform.get("rotation"),
-                transform.get("translation"),
-                transform.get("scale3D")
-            ) {
-                let (rot_w, rot_x, rot_y, rot_z) = parse_rotation(rotation);
-                let (trans_x, trans_y, trans_z) = parse_xyz(translation);
-                let (scale3d_x, scale3d_y, scale3d_z) = parse_xyz(scale3d);
-           
+            // Parse location
+            if let Some(location) = transform.get("location") {
+                let (trans_x, trans_y, trans_z) = parse_xyz(location);
+                
+                // Parse rotation
+                let (rot_x, rot_y, rot_z) = transform.get("rotation")
+                    .map(parse_xyz)
+                    .unwrap_or((0.0, 0.0, 0.0));
+                
+                // Parse scale3D
+                let (scale3d_x, scale3d_y, scale3d_z) = transform.get("scale3D")
+                    .map(parse_xyz)
+                    .unwrap_or((1.0, 1.0, 1.0));
+
                 // Create or update the transform
                 let mut transform = player.transform.take().unwrap_or_else(|| Transform {
                     rotation: None,
@@ -117,33 +118,36 @@ pub fn update_player_location(socket: SocketRef, data: Data<Value>, players: Arc
                     scale3D: Scale3D { x: 1.0, y: 1.0, z: 1.0 },
                     location: None,
                 });
-           
-                // Update rotation
-                transform.rotation = Some(Rotation { w: rot_w, x: rot_x, y: rot_y, z: rot_z });
-           
-                // Update translation
+
+                // Update rotation (note: we're missing 'w' component, so we'll set it to 1.0)
+                transform.rotation = Some(Rotation { w: 1.0, x: rot_x, y: rot_y, z: rot_z });
+
+                // Update translation and location
                 let new_translation = Translation { x: trans_x, y: trans_y, z: trans_z };
                 transform.translation = Some(new_translation.clone());
                 transform.location = Some(new_translation);
-           
+
                 // Update scale3D
                 transform.scale3D = Scale3D { x: scale3d_x, y: scale3d_y, z: scale3d_z };
+
                 // Update the player's transform
                 player.transform = Some(transform);
-           
+
                 // Parse player movement axis values
                 if let Some(move_action) = data.0.get("move Action Value") {
                     let (mv_action_value_x, mv_action_value_y) = parse_xy(move_action);
                     player.moveActionValue = Some(MoveActionValue { x: mv_action_value_x, y: mv_action_value_y });
                 }
-                if let Some(controlRotation) = data.0.get("control Rotation") {
-                    let (ctrl_rot_x, ctrl_rot_y, ctrl_rot_z) = parse_xyz(controlRotation);
+
+                if let Some(control_rotation) = data.0.get("control Rotation") {
+                    let (ctrl_rot_x, ctrl_rot_y, ctrl_rot_z) = parse_xyz(control_rotation);
                     player.controlRotation = Some(Vec3D { x: ctrl_rot_x, y: ctrl_rot_y, z: ctrl_rot_z });
                 }
+
                 // Print a debug statement
                 println!("Updated player location: {:?}", player);
             } else {
-                println!("Invalid transform data structure");
+                println!("Invalid transform data structure: missing location");
             }
         } else {
             println!("Player not found: {}", socket.id);
@@ -151,6 +155,7 @@ pub fn update_player_location(socket: SocketRef, data: Data<Value>, players: Arc
     } else {
         println!("Failed to parse location: transform field not found or is not an object");
     }
+
     // Send a reply containing the correct data
     socket.emit("messageBack", data.0).ok();
 }
