89b17d3a58a97f331d3b87157c3cff3098fb5772
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-10-16 00:07:37 -0400

    Update lib.rs

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 extern crate plugin_test_api;
 
 mod english;
@@ -38,38 +37,3 @@ pub use core::PluginMetadataType as CorePluginMetadataType;
 
 // Export other necessary items
 pub use stars_beyond::{get_plugin as get_stars_beyond_plugin, get_plugin_metadata as get_stars_beyond_metadata};
-=======
-// this file is generated, do not edit manually
-// (or edit it if you want, it's not like I can prevent you from doing it)
-
-extern crate plugin_test_api;
-
-#[macro_use] extern crate nom;
-mod english;
-mod french;
-mod greek;
-mod core;
-
-pub use english::*;
-pub use french::*;
-pub use greek::*;
-pub use core::*;
-
-use plugin_test_api::PluginInformation;
-use std::collections::hash_map::HashMap;
-
-pub struct Plugins {
-    pub list: HashMap<String, Box<dyn PluginInformation>>,
-}
-
-pub fn plugins() -> Plugins {
-    let mut h: HashMap<String, Box<dyn PluginInformation>> = HashMap::new();
-
-    h.insert("english".to_string(), Box::new(english::PLUGIN_METADATA));
-    h.insert("french".to_string(), Box::new(french::PLUGIN_METADATA));
-    h.insert("greek".to_string(), Box::new(greek::PLUGIN_METADATA));
-    h.insert("core".to_string(), Box::new(core::PLUGIN_METADATA));
-
-    Plugins { list: h }
-}
->>>>>>> origin/main
