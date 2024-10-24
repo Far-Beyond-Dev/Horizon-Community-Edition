5c69177370b777fdb381bd4565142b78eb7a9d59
xandr. <xxdr@duck.com>
2024-10-23 21:55:40 -0500

    feat: AIO installer for GNU/Linux
    haven't tested

--- installers-deb.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y clang libsqlite3-dev libssl-dev golang curl libclang-dev gcc-multilib
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- installers-ubuntu.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y clang libsqlite3-dev libssl-dev build-essentials golang rustup curl libclang-dev gcc-multilib libsqlite3-dev libclang-dev
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
